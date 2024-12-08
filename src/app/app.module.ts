233e8de99c710d925ccb547a3070e8f3dc8df00e
Yunier <yunierab88@gmail.com>
2024-12-08 15:21:15 -0300

    Terminada la primera pantalla con binding de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { ContentFormDataComponent } from './componentes/content-form-data/content-form-data.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContentFormDataComponent } from './componentes/content-form-data/conten
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
--- src/app/componentes/content-form-data/content-form-data.component.ts
+++ src/app/componentes/content-form-data/content-form-data.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content-form-data',
   templateUrl: './content-form-data.component.html',
   styleUrl: './content-form-data.component.css'
 })
-export class ContentFormDataComponent {
+export class ContentFormDataComponent implements OnInit
+{
+   inputSalarioNominal: number = 0;
+   inputCheckHijosACargo: boolean = false;
+   inputCheckConyugueACargo: boolean = false;
+   inputPorcientoDeduccionPersonasCargo: number = 0;
+   inputCantidadHijosSinDiscapacidad: number = 0;
+   inputCantidadHijosConDiscapacidad: number = 0;
+   inputAportaFondoSolidaridad: number = 0;
+   inputCheckAdicionalFondoSolidaridad: boolean = false;
+   inputAporteMensualCajaNotarial: number = 0;
+   inputOtrasDeducciones: number = 0;
+
+  constructor() {
+  }
+
+  ngOnInit(): void
+  {
+
+  }
+
+  mostrarDatosFormulario(): void
+  {
+      console.log("Salario nominal: ", this.inputSalarioNominal);
+      console.log("Tiene hijos a cargo: ", this.inputCheckHijosACargo);
+      console.log("Tiene cónyugue a cargo: ", this.inputCheckConyugueACargo);
+      console.log("Porcentaje de deducción de las personas a cargo: ", this.inputPorcientoDeduccionPersonasCargo);
+      console.log("Cantidad de hijos sin discapacidad: ", this.inputCantidadHijosSinDiscapacidad);
+      console.log("Cantidad de hijos con discapacidad: ", this.inputCantidadHijosConDiscapacidad);
+      console.log("Aporta fondo solidaridad: ", this.inputAportaFondoSolidaridad);
+      console.log("Adicional fondo solidaridad: ", this.inputCheckAdicionalFondoSolidaridad);
+      console.log("Aporte mensual a CJPPU o Caja Notarial: ", this.inputAporteMensualCajaNotarial);
+      console.log("Otras deducciones: ", this.inputOtrasDeducciones);
+  }
 
 }
